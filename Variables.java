3c02345d8a40418f1ffc9b4ed958bb2e56a800f4
XBS17 <46597227+XBS17@users.noreply.github.com>
2019-11-18 18:28:27 +0100

    Java Exercises
    Este es un repositorio donde iré subiendo ejercicios de un curso de Java.

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+	
+	public static void main (String[] args) {
+		
+		
+		byte edad=35; //declaración e iniciación de variable en una línea
+		
+		/*La siguiente instrucción imprime en la consola del sistema el valor
+		 * de la variable edad*/
+		
+		System.out.println(edad);
+		
+		edad=75;
+		
+		System.out.println(edad);
+		
+		}
+	
+	
+	}
